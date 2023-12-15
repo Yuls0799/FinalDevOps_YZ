6cfaca3aaedd8c5a3e6f045e91cebd9d381bca5d
Yuliana Zorrilla <107286036+Yuls0799@users.noreply.github.com>
2023-12-15 16:38:49 -0400

    Add yml and change tests.js

--- tests.test.js
+++ tests.test.js
@@ -6,7 +6,6 @@ describe('Test Greeting', () => {
         h1.textContent = 'Buenas!, esta es la práctica final de  Yuliana Zorrilla 2021-1823';
         body.appendChild(h1);
   
-        // Asegúrate de realizar la aserción en el texto del h1
         expect(h1.textContent).toContain('Buenas!, esta es la práctica final de  Yuliana Zorrilla 2021-1823');
       } else {
         // Advertencia
